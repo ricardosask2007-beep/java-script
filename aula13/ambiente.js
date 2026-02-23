04aaf003fdb772aac8bdd89ecf315617e22ef87e
Ricardo Carvalho <240542915+ricardosask2007-beep@users.noreply.github.com>
2026-02-23 15:17:24 -0300

    repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+//testa primeiro e executa depois
+let c = 1
+while (c <= 6) {
+    console.log(c);
+    c++;
+}
+console.log ('---------------')
+//executa primeiro e testa depois
+let i = 1
+do {
+    console.log(i);
+    i++
+} while (i <= 6)
