2a63e86b997080163c31965b7b5fbf745a874926
Ricardo Carvalho <240542915+ricardosask2007-beep@users.noreply.github.com>
2026-02-24 15:49:56 -0300

    repetições

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,24 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] faltam dados!')
+    } else {
+        res.innerHTML = 'Contando:'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            for(let c = i; c <= f; c += p ) {
+                res.innerHTML += ` ${c} ` 
+            }
+        } else {
+            for(let c = i; c >= f ; c -= p) {
+                res.innerHTML += ` ${c} ` 
+            }
+        }
+    }
+}
